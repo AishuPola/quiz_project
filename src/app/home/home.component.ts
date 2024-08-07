60a09782fedcb27633d2b1d50cab88e0b9abc213
srujan1306 <srujan.pothu@proclink.com>
2024-08-07 11:35:40 +0530

    buttons shifted to home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -3,6 +3,7 @@ import { QuestionsComponent } from '../questions/questions.component';
 import { QuizService } from '../quiz.service';
 import { ActivatedRoute } from '@angular/router';
 import { ScoreComponent } from '../score/score.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -13,10 +14,37 @@ import { ScoreComponent } from '../score/score.component';
 })
 export class HomeComponent {
   question: any;
-  constructor(public quizService: QuizService, private route: ActivatedRoute) {
+  id = 0;
+  constructor(
+    public quizService: QuizService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {
     let id: any = this.route.snapshot.paramMap.get('id') as string; // From URL
 
     this.question = this.quizService.questions[id - 1];
     console.log(this.question);
+  }
+  prevQuestion() {
+    if (this.id > 1) {
+      this.id--;
+      this.question = this.quizService.questions[this.id];
+
+      this.router.navigate([`question/${this.id}`]);
+    }
+  }
+  nextQuestion() {
+    if (this.id <= this.quizService.questions.length - 1) {
+      this.id++;
+      this.question = this.quizService.questions[this.id];
+
+      this.router.navigate([`question/${this.id}`]);
+    } else {
+      this.onSubmit();
+    }
+  }
+
+  onSubmit() {
+    this.router.navigate(['/score']);
   }
 }
