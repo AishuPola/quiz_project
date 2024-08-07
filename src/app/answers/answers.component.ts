8a146fbdf3ec804de2b90f61c972648509adb2a9
SriManaswini <puli.manaswini@proclink.com>
2024-08-07 17:22:53 +0530

    answers-comp

---
+++ src/app/answers/answers.component.ts
@@ -0,0 +1,12 @@
+import { Component,Input } from '@angular/core';
+
+@Component({
+  selector: 'app-answers',
+  standalone: true,
+  imports: [],
+  templateUrl: './answers.component.html',
+  styleUrl: './answers.component.scss'
+})
+export class AnswersComponent {
+  @Input() question: any;
+}
